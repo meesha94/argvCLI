567141e084d09ad4e6c7d51eea329c1f10b87592
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-01-31 14:15:21 +0000

    added multi movie option

--- src/app.js
+++ src/app.js
@@ -7,6 +7,13 @@ const app = (args) => {
             movie.add();
             console.log(movieArr);
             break;
+        case "add multi":
+            const movie1 = new Movie(args[3], args[4]);
+            const movie2 = new Movie(args[5] ,args[6]);
+            movie1.add();
+            movie2.add();
+            console.log(movieArr);
+            break;
         default:
             console.log("Incorrect command");
     }
